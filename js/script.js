c005894ecd4bcb61d0c6fc8e197b9066c74de6f7
Yong-coder <76443343+Yong-coder@users.noreply.github.com>
2024-02-21 18:02:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-	// Sticky Navbar
-
+		// Sticky Navbar
+    let header = document.querySelector('header');
     let menu = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
      
